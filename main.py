ee60f2238fb37ba70cb441b9798d2e87f69d8908
bighammer <3225579752@qq.com>
2023-04-29 19:40:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -180,8 +180,8 @@ def login(user, password):
 def main(_user, _passwd, min_1, max_1):
     user = str(_user)
     password = str(_passwd)
-    min_1 = 12000
-    max_1 = 22300
+    min_1 = 18000
+    max_1 = 26300
      
     step = str(random.randint(min_1, max_1))
     print("已设置为随机步数(" + str(min_1) + "~" + str(max_1) + ")")
